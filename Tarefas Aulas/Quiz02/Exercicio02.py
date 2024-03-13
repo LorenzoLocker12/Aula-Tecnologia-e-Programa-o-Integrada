4015c019d41de8669de4f2d8f8affab8ae2b71ea
Lorenzo Locker <127125142+LorenzoLocker12@users.noreply.github.com>
2024-03-13 19:49:49 -0300

    Delete Tarefas Aulas directory

--- Tarefas Aulas/Quiz02/Exercicio02.py
+++
@@ -1,9 +0,0 @@
-frase = input("Digite a frase: ")
-remover = input("\nDigite a letra que quer substituir: ")
-novaLetra = input("\nDigite a letra que quer colocar no local da antiga letra: ")
-
-for i in frase:
-    if i in remover:
-        frase = frase.replace(remover, novaLetra)
-
-print(f"\n{frase}")
